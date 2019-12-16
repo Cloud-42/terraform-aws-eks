4af7e58a9465d2afef93ea2ef01737a258b52299
Cloud-42 <hello@cloud42.io>
2019-12-16 09:31:55 +0000

    Added Node SG output

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
 
+output "node_sg_id" {
+  value = module.eks-security.node_sg_id
+}
+
